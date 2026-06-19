7c81b8baf179c308367d971929eb5894f9999efa
Hai118 <quachhai11826@gmail.com>
2026-06-19 09:00:00 +0700

    fix(auth): read Gmail credentials dynamically and detect placeholders

--- src/java/registrant/service/RegistrantTrackProfileService.java
+++
@@ -1,8 +0,0 @@
-package registrant.service;
-
-import auth.dto.UserDTO;
-import jakarta.servlet.http.HttpServletRequest;
-
-public interface RegistrantTrackProfileService {
-    void copyTrackingToRequest(UserDTO user, HttpServletRequest request);
-}
