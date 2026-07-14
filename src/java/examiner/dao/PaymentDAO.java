fcc653cfd655a451d51910c8038f702c873171bf
minh <minhphamnhat2852006@gmail.com>
2026-07-14 19:46:27 +0700

    Refactor examiner DAO interfaces naming batch 4

--- src/java/examiner/dao/PaymentDAO.java
+++ src/java/examiner/dao/PaymentDAO.java
@@ -1,21 +1,13 @@
 package examiner.dao;
 
-
-
 import shared.model.Payment;
 
-
-
+// DAO contract for Payment persistence; examiner module SQL boundary.
 public interface PaymentDAO {
 
+    // Inserts a payment row for one exam enrollment.
+    boolean add(Payment payment);
 
-
-    boolean insert(Payment payment);
-
-
-
+    // Returns true when a completed payment exists for one enrollment.
     boolean hasCompletedPayment(int examEnrollmentId);
-
 }
-
-
