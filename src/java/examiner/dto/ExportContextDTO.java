ed205dbf23d2a570eec7229eb061454859bf7cee
minh <minhphamnhat2852006@gmail.com>
2026-07-15 20:57:19 +0700

    Update examiner export DTOs

--- src/java/examiner/dto/ExportContextDTO.java
+++ src/java/examiner/dto/ExportContextDTO.java
@@ -1,9 +1,9 @@
 package examiner.dto;
 
+import shared.enums.SectionType;
 import shared.model.ExaminerSchedule;
 
+// Export/print session context passed from examiner controllers to document services.
 public record ExportContextDTO(int examId, ExaminerSchedule schedule,
-        boolean isTheory, String sectionName) {
-
+        boolean isTheory, SectionType section) {
 }
-
